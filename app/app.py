1a1ca40af8b7444326bd367a5b0d679bfc0c67b6
Awez <awezacademic@gmail.com>
2025-07-11 17:58:48 +0530

    Added datasets and updated flask app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,26 @@
 from flask import Flask,request,render_template,redirect
-import _mysql_connector as sql
-
-cur=sql.cursor()
-
+import pickle
 
 app=Flask(__name__)
 
-@app.route("/")
-def index():
+vect_f=open('vectorizer.pkl','rb')
+model_f=open('spam_model.pkl','rb')
+
+vectorizer=pickle.load(vect_f)
+model=pickle.load(model_f)
+
+@app.route('/',methods=['GET','POST'])
+def check():
+
+    if(request.method =='POST'):
+        msg=request.form.get('message')
+        msg=[msg]
+        msg_fin=vectorizer.transform(msg)
+
+        res=model.predict(msg_fin)
+
+        if(res[0]==1):
+            return render_template('index.html',prediction='Spam')
+        else:
+            return render_template('index.html',prediction='Not Spam')
     return render_template('index.html')
